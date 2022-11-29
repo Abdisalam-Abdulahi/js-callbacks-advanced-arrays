2d519e2d678146947725f8599f842596aaba0e62
Abdisalam-Abdulahi <105162769+Abdisalam-Abdulahi@users.noreply.github.com>
2022-11-29 13:02:20 -0800

    massive changes

--- callbacks.js
+++ callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
   //Given this problem: 
@@ -12,39 +12,60 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
     return cb(arr[0]);
-  }
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
+ } Function invocation 
+  // firstItem(items, function(first) {
+  //   console.log(first)
+  // });
 */
-
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function (arrLength) {
+  console.log(arrLength);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(items[items.length - 1]);
 }
+last(items, function (lastOne) {
+  console.log(lastOne);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(2,3,function(addition){
+  console.log(addition)
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y)
 }
+multiplyNums(2,4,function(multiplication){
+  console.log(multiplication)
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)){
+    return cb(true)
+  }else{
+    return cb(false)
+  }
 }
+contains("banana",["banana","mango","watermelon"], function(check){
+console.log(check)
+})
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
+//function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+  // Do not mutate the original array.}
